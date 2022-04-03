6e1a0f2247400f52758699e6487a3c057c4d6814
roma_marshall <roma_marshall@outlook.com>
2022-04-03 23:02:48 +0200

    Init commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <main>
+    <div class="text-center my-10 text-xl text-gray-500">Landing Page</div>
+  </main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+
+</style>
